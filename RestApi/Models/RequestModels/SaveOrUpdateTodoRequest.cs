ad6508e348a217e8b26815b9262e5baa215dd6b9
OlegasDav <84792738+OlegasDav@users.noreply.github.com>
2021-09-02 20:50:52 +0300

    Added files

---
+++ RestApi/Models/RequestModels/SaveOrUpdateTodoRequest.cs
@@ -0,0 +1,21 @@
+﻿using Contracts.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestApi.Models.RequestModels
+{
+    public class SaveOrUpdateTodoRequest
+    {
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public Difficulty Difficulty { get; set; }
+
+        public DateTime DateCreated { get; set; }
+
+        public bool IsDone { get; set; }
+    }
+}
