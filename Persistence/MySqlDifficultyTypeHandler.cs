d88a6f41d71758f0b5981f3f51174af8b43deb46
Oleg <oldav0722@gmail.com>
2021-09-07 19:49:48 +0300

    Created MySqlDifficultyTypeHandler.cs
    Not working

---
+++ Persistence/MySqlDifficultyTypeHandler.cs
@@ -0,0 +1,24 @@
+﻿using Contracts.Enums;
+using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence
+{//NEVEIKIA!!!
+    public class MySqlDifficultyTypeHandler : SqlMapper.TypeHandler<Difficulty>
+    {
+        public override Difficulty Parse(object value)
+        {
+            return (Difficulty)Enum.Parse(typeof(Difficulty), (string)value, true);
+        }
+
+        public override void SetValue(IDbDataParameter parameter, Difficulty value)
+        {
+            parameter.Value = value.ToString();
+        }
+    }
+}
