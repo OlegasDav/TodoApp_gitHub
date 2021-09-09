49d7001641b596ecf546e6ba85ca9783eefbd29e
Oleg <oldav0722@gmail.com>
2021-09-09 11:12:30 +0300

    Add new class

---
+++ RestApi/Models/RequestModels/UpdateStatusRequest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestApi.Models.RequestModels
+{
+    public class UpdateStatusRequest
+    {
+        public bool IsDone { get; set; }
+    }
+}
