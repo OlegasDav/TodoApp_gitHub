eed2f28dcedfb07c120d7c645efe3f5368f49566
Oleg <oldav0722@gmail.com>
2021-09-15 15:14:23 +0300

    Added attribute

---
+++ RestApi/Attributes/ApiKeyAttribute.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Persistence.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+
+
+namespace RestApi.Attributes
+{
+    public class ApiKeyAttribute : Attribute, IAsyncActionFilter
+    {
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+
+            //var todoId = (Guid)context.ActionArguments["Id"];
+            var key = context.HttpContext.Request.Headers["ApiKey"].SingleOrDefault();
+
+            if (string.IsNullOrWhiteSpace(key))
+            {
+                context.Result = new BadRequestObjectResult("ApiKey header is missing");
+
+                return;
+            }
+
+            var userRepository = context.HttpContext.RequestServices.GetService<IUserRepository>();
+            var apiKey = await userRepository.GetApiKeyAsync(key);
+
+            if (apiKey is null)
+            {
+                context.Result = new NotFoundObjectResult("ApiKey is not found");
+
+                return;
+            }
+
+            if (!apiKey.IsActive)
+            {
+                context.Result = new BadRequestObjectResult("ApiKey expired");
+
+                return;
+            }
+
+            context.HttpContext.Items.Add("userId", apiKey.UserId);
+
+            await next();
+        }
+    }
+}
