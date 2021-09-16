11eb630d3933046d44c21aabd5639526a99d7a07
Oleg <oldav0722@gmail.com>
2021-09-16 21:20:49 +0300

    Added Domain

---
+++ Domain/Services/ApiKeyService.cs
@@ -0,0 +1,36 @@
+﻿using Persistence.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+
+namespace Domain.Services
+{
+    public class ApiKeyService : IApiKeyService
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly int _apiKeyLimit;
+
+        public ApiKeyService(IUserRepository userRepository, IConfiguration configuration)
+        {
+            _userRepository = userRepository;
+            _apiKeyLimit = Convert.ToInt32(configuration.GetSection("AppSettings")["ApiKeyLimits"]);
+        }
+
+        public async Task<bool> CheckApiKeyLimitAsync(Guid userId)
+        {
+            var apiKeys = await _userRepository.GetUserApiKeysAsync(userId);
+
+            var apiKeyCount = apiKeys.ToList().Count;
+
+            if (_apiKeyLimit > apiKeyCount)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ Domain/ServicesExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Domain.Services;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Domain
+{
+    public static class ServicesExtensions
+    {
+        public static IServiceCollection AddDomain(this IServiceCollection services)
+        {
+            return services.AddServices();
+        }
+
+        private static IServiceCollection AddServices(this IServiceCollection services)
+        {
+            return services.AddSingleton<IApiKeyService, ApiKeyService>();
+        }
+    }
+}
